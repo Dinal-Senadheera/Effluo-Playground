2adda47ed3dc4fa9fee1c2a94b4ee876c23b496b
ch4mi2 <chamithu4@gmail.com>
2025-03-19 00:09:53 +0530

    commit

--- calculateTotal.js
+++ calculateTotal.js
@@ -1,4 +1,4 @@
-function computeTotal(items, discount) { 
+function computeTotal(items, discount) {
     let subtotal = items.reduce((sum, item) => sum + item.price, 0);
     let discountedTotal = subtotal - discount;
     return discountedTotal;
@@ -7,4 +7,49 @@ function computeTotal(items, discount) {
 let items = [{ name: "Book", price: 20 }, { name: "Pen", price: 5 }];
 let discount = 5;
 
-console.log(computeTotal(items, discount)); 
+let userName = "John Doe";
+let userAge = 30;
+let isUserLoggedIn = true;
+const maxLoginAttempts = 5;
+
+function checkLoginStatus() {
+    if (isUserLoggedIn) {
+        console.log(`${userName} is logged in.`);
+    } else {
+        console.log(`${userName} is not logged in.`);
+    }
+}
+
+function updateUserAge(newAge) {
+    userAge = newAge;
+    console.log(`${userName}'s new age is ${userAge}`);
+}
+
+function attemptLogin(attempts) {
+    if (attempts <= maxLoginAttempts) {
+        console.log(`Login attempt ${attempts} successful.`);
+    } else {
+        console.log("Too many login attempts. Please try again later.");
+    }
+}
+
+for (let i = 1; i <= 7; i++) {
+    attemptLogin(i);
+}
+
+updateUserAge(31);
+checkLoginStatus();
+
+let items2 = ["apple", "banana", "cherry", "date"];
+console.log("Items:", items2);
+
+function addItem(item) {
+    items2.push(item);
+    console.log(`${item} has been added to the list.`);
+}
+
+addItem("elderberry");
+
+console.log("Updated Items:", items2);
+
+console.log(calculateTotal(items, discount)); 
