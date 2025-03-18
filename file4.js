057a7284e41ab7a5f84b004383eefbaad3a8024d
ShenanPerera <it21214752@my.sliit.lk>
2025-03-18 22:09:14 +0530

    feat: EMERGENCY - Hotfix for critical authentication bypass vulnerability (p0)

---
+++ file4.js
@@ -0,0 +1,7 @@
+// file4.js: Object creation and access
+const person = {
+    name: "Alice",
+    age: 30,
+    city: "New York"
+  };
+  console.log(person.name);
---
+++ file5.js
@@ -0,0 +1,7 @@
+// file5.js: Conditional statement
+const age = 20;
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
