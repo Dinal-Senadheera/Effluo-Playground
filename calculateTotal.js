7f74519c23ca271f389b9dc79b3c06bf4624e6f8
ch4mi2 <chamithu4@gmail.com>
2025-03-19 00:24:32 +0530

    commit

--- calculateTotal.js
+++ calculateTotal.js
@@ -1,10 +1,10 @@
 function computeTotal(items, discount) {
-    let subtotal = items.reduce((sum, item) => sum + item.price, 0);
+    let subtotal = items.reduce((sum, item) => sum + item.price, 0); 
     let discountedTotal = subtotal - discount;
     return discountedTotal;
 }
 
-let items = [{ name: "Book", price: 20 }, { name: "Pen", price: 5 }];
+let items = null;  
 let discount = 5;
 
 let userName = "John Doe";
